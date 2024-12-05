0d12bfe324636f619d6c3c2a703ddc2c2a48efea
aslam <aslam@smarthatch.com>
2024-12-05 07:57:16 +0530

    config file changed

--- src/components/config.js
+++ src/components/config.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL =  "https://api.thequickapp.com/api/"
-export const API_IMAGE_URL = "https://api.thequickapp.com/"
+export const API_BASE_URL =  "https://thequickapp.com/api/"
+export const API_IMAGE_URL = "https://thequickapp.com/"
